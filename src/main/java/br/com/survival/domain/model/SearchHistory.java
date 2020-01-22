ca92c17003d854e88e818bdc235eb0416e78bf2f
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-01-22 09:06:23 -0300

    Ajuste de identação.

--- src/main/java/br/com/survival/domain/model/SearchHistory.java
+++ src/main/java/br/com/survival/domain/model/SearchHistory.java
@@ -25,7 +25,7 @@ public class SearchHistory {
 	
 	@Id
 	@GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+        @GenericGenerator(name = "uuid2", strategy = "uuid2")
 	@Column(name = "codigo")
 	private UUID code;
 	
