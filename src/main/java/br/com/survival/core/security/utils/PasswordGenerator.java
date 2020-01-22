f0f457c8e50fe958e83ec3d855165d2c2fb2bfc8
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-01-22 08:52:57 -0300

    Ajuste de identação.

--- src/main/java/br/com/survival/core/security/utils/PasswordGenerator.java
+++ src/main/java/br/com/survival/core/security/utils/PasswordGenerator.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
 	
-	public static void main(String ... args) {
+    public static void main(String ... args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         System.out.println(encoder.encode("admin"));
     }
