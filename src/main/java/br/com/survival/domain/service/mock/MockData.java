e1eb9bb466f17425fdcb16619332d82e98612d37
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-01-22 09:03:09 -0300

    Ajuste de identação.

--- src/main/java/br/com/survival/domain/service/mock/MockData.java
+++ src/main/java/br/com/survival/domain/service/mock/MockData.java
@@ -10,40 +10,40 @@ import java.util.UUID;
 public class MockData {
 	
 	private MockData() {
-		throw new IllegalStateException("Utility class");
+	    throw new IllegalStateException("Utility class");
 	}
 	
 	public static String getRandomCode() {
-		return UUID.randomUUID().toString();
+	    return UUID.randomUUID().toString();
 	}
 	
 	public static BigDecimal getRandomBigDecimal() {
-		BigDecimal min = BigDecimal.valueOf(100D);
-		BigDecimal max = BigDecimal.valueOf(5000D);
+	    BigDecimal min = BigDecimal.valueOf(100D);
+	    BigDecimal max = BigDecimal.valueOf(5000D);
 		
 	    BigDecimal randomBigDecimal = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
 	    return randomBigDecimal.setScale(2, RoundingMode.HALF_UP);
 	}
 	
 	public static OffsetDateTime getRandomDateTime() {
-		OffsetDateTime startDateTime = OffsetDateTime.parse("2019-12-01T00:00:00+00:00");
+	    OffsetDateTime startDateTime = OffsetDateTime.parse("2019-12-01T00:00:00+00:00");
 	    OffsetDateTime latestDateTime = OffsetDateTime.now();	    
 	    Random random = new Random(0);
 	        
-		long differenceInSeconds = startDateTime.until(latestDateTime, ChronoUnit.SECONDS);
-        long seconds = nextLong(random, differenceInSeconds);
-        return startDateTime.plusSeconds(seconds);
+	    long differenceInSeconds = startDateTime.until(latestDateTime, ChronoUnit.SECONDS);
+            long seconds = nextLong(random, differenceInSeconds);
+            return startDateTime.plusSeconds(seconds);
 	}
 	
 	private static long nextLong(Random rng, long n) {
-        long bits, val;
+            long bits, val;
         
-        do {
-           bits = (rng.nextLong() << 1) >>> 1;
-           val = bits % n;
-        } while (bits - val + (n - 1) < 0L);
+            do {
+                bits = (rng.nextLong() << 1) >>> 1;
+                val = bits % n;
+            } while (bits - val + (n - 1) < 0L);
         
-        return val;
-    } 
+            return val;
+        } 
 
 }
