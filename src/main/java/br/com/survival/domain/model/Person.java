e55412594ebb3d04dcf53905bacd9d1c1ee8f514
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-01-22 09:06:50 -0300

    Ajuste de identação.

--- src/main/java/br/com/survival/domain/model/Person.java
+++ src/main/java/br/com/survival/domain/model/Person.java
@@ -30,7 +30,7 @@ public class Person {
 	private String cpf;
 	
 	@GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+        @GenericGenerator(name = "uuid2", strategy = "uuid2")
 	@Column(name = "codigo")
 	private UUID code;
 	
